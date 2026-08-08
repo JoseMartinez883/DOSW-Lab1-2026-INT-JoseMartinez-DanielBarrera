828759ed6afda630a4d9234ffaf630cec1a5fd9a
Jose Martinez <115952610+JoseMartinez883@users.noreply.github.com>
2026-08-08 02:07:58 -0500

    feat: features student A

---
+++ src/main/java/Laboratorio1/Reto4/hashUtils.java
@@ -0,0 +1,34 @@
+package Laboratorio1.Reto4;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class hashUtils {
+
+    public static Map<String,Integer> listElementsToHashMap(List<Elemento> elements){
+
+        Map<String,Integer> mapElements = elements.stream()
+                .collect(Collectors.toMap(
+                        Elemento::getElement,
+                        Elemento::getValue,
+                        (existente,nuevo) -> existente
+                ));
+        return mapElements;
+    }
+
+    public static Map<String, Integer> combineHashMapAndHashTable(HashMap<String, Integer> hashMap, Hashtable<String, Integer> hashtable) {
+
+        Map<String, Integer> combinado = new HashMap<>(hashMap);
+        combinado.putAll(hashtable);
+
+        return combinado.entrySet().stream()
+                .map(entry -> Map.entry(entry.getKey().toUpperCase(), entry.getValue()))
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (existente, nuevo) -> existente,
+                        LinkedHashMap::new
+                ));
+    }
+}
